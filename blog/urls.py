95d695ca197b28c2f8d9af6a6bfc8c33d4745425
MikeMaroni9 <k.vaitka@gmail.com>
2023-09-11 15:18:57 +0000

    post_details.html added, ability to open posts from index page.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import Home, PostDetail
 
 urlpatterns = [
     path('', Home.as_view(), name="index"),
+    path('detail/<int:pk>', PostDetail.as_view(), name="postdetail"),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .models import Post
 class Home(ListView):
     model = Post
     template_name = 'index.html'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
