5ed1f882849ee30e20549ba9c928504e39e2ee66
MikeMaroni9 <k.vaitka@gmail.com>
2023-09-12 22:14:10 +0000

    ability to edit and delete posts added only to authenticated users, also create a new posts from navbar have been added only to authenticated users

--- p4redditblog/settings.py
+++ p4redditblog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'sdfsd09823jSDADFmkl23412'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mikemaroni9-p4redditdja-o8339snscw4.ws-eu104.gitpod.io', 'localhost', '8000-mikemaroni9-p4redditdja-d7cv4wk94km.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['8000-mikemaroni9-p4redditdja-o8339snscw4.ws-eu104.gitpod.io', 'localhost', '8000-mikemaroni9-p4redditdja-qxbqbllf7dw.ws-eu104.gitpod.io']
 
 
 # Application definition
