77c92ce204a45bab2490173b666b1342222b595a
MikeMaroni9 <k.vaitka@gmail.com>
2023-09-13 14:54:53 +0000

    Comment view added to post_detail page

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.urls import reverse_lazy
+from .forms import CommentForm
 
 class Home(ListView):
     model = Post
@@ -11,6 +12,27 @@ class Home(ListView):
 class PostDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+    def get(self, request, pk, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, pk=pk)
+        comments = post.comments.filter(approved=True).order_by("created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
 
 class AddPost(CreateView):
     model = Post
